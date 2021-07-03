dae003db071f0ec30172202bb598f2cd6c1ebc20
marialauragodoy <85503509+marialauragodoy@users.noreply.github.com>
2021-07-03 09:54:20 -0300

    Add files via upload

---
+++ PHP1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $nombre = "Maria Laura";
+    $apellido = "Godoy";
+    $edad = 22;
+    $hobbie = "Leer";
+    $editor = "Visual Studio Code";
+    $so = "Windows 11";
+
+    echo "Mi nombre es ".$nombre." ".$apellido."<br>";
+    echo "Tengo ".$edad."\n";
+    echo "Me gusta mucho ".$hobbie." en mis tiempos libres<br>";
+    echo "Mi editor de código preferido es ".$editor."<br>";
+    echo "Y el sistema operativo que uso es ".$so."<br>";
+?>
+</body>
+</html>
